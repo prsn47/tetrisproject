8aa900d3d9f4d7dbeeed300cda01bf1f4690d0b0
idkgoonahh <brickloop238@gmail.com>
2026-05-31 13:39:24 -0600

    first

---
+++ src/colors.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <raylib.h>
+#include <vector>
+
+extern const Color darkGray;
+extern const Color red;
+extern const Color orange;
+extern const Color yellow;
+extern const Color green;
+extern const Color cyan;
+extern const Color blue;
+extern const Color purple;
+extern const Color lightGray;
+extern const Color gray;
+
+std::vector<Color> GetCellColors();
